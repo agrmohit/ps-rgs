da66955b3ca15f7c0b8214a80e9c16ee0b90fc13
Mohit Raj <agrmohit@outlook.com>
2020-04-25 22:22:06 +0530

    Finished development of version 0.1.0 of 'The GitHub Cards App'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Axios from "axios";
 
-const App = ({ title }) => <div className="header">{title}</div>;
+const CardList = (props) => (
+  <div>
+    {props.profiles.map((profile) => (
+      <Card key={profile.id} {...profile} />
+    ))}
+  </div>
+);
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} alt="User GitHub Profile pic" />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { username: "" };
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await Axios.get(
+      `https://api.github.com/users/${this.state.username}`
+    );
+    this.props.onSubmit(resp.data);
+    this.setState({ username: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub username"
+          value={this.state.username}
+          onChange={(event) => this.setState({ username: event.target.value })}
+          required
+        />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+
+  addNewProfile = (profileData) => {
+    this.setState((prevState) => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
